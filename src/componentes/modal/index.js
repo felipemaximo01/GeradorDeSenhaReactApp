18aa026f2704f88ab06a9419ae8c443fec192aa7
Felipe Maximo <felipecontato2001@hotmail.com>
2024-04-07 20:04:20 -0300

    Copiando senha

--- src/componentes/modal/index.js
+++ src/componentes/modal/index.js
@@ -1,15 +1,23 @@
 import { View,Text,StyleSheet,TouchableOpacity,Pressable } from "react-native";
+import * as Clipboard from 'expo-clipboard'
 
-export function ModalPassword(){
+export function ModalPassword({password,handleClose}){
+
+    async function handleCopyPassword(){
+        await Clipboard.setStringAsync(password)
+        alert("Senha salva com sucesso")
+        handleClose();
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.title}>Senha Gerada</Text>
-                <Pressable style={styles.innerPassword}>
-                    <Text style={styles.text}>123415678</Text>
+                <Pressable style={styles.innerPassword} onLongPress={handleCopyPassword}>
+                    <Text style={styles.text}>{password}</Text>
                 </Pressable>
                 <View  style={styles.buttonArea}>
-                    <TouchableOpacity style={styles.button}>
+                    <TouchableOpacity style={styles.button} onPress={handleClose}>
                         <Text style={styles.buttonText}>Voltar</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.button, styles.buttonSave]}>
